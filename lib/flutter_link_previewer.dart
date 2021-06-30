d4953cdda112450c0c2cb2ac278881321a419daa
Rahul Chandera <rahul.chandera@dynamicelements.no>
2021-06-30 19:10:41 +0530

    exposed getPreviewData method of util

--- lib/flutter_link_previewer.dart
+++ lib/flutter_link_previewer.dart
@@ -3,4 +3,5 @@ library flutter_link_previewer;
 export 'src/types.dart';
 export 'src/utils.dart' show REGEX_LINK;
 export 'src/utils.dart' show ImagePosition;
+export 'src/utils.dart' show getPreviewData;
 export 'src/widgets/link_preview.dart';
